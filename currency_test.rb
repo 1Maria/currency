54785c49b1eafbeab9dcfc95668b30c1facb971c
Maria <mliberov@gmail.com>
2015-01-20 17:08:33 -0500

    Add amount test

--- currency_test.rb
+++ currency_test.rb
@@ -6,4 +6,10 @@ class CurrencyTest < MiniTest::Test
   def test_currency_class_exists
     assert Currency
   end
+
+  def test_created_with_amount
+    currency = Currency.new(3)
+    assert currency.amount == 3
+  end
+
 end
